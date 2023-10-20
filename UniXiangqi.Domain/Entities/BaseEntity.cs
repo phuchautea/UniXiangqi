354683058d84240ed34f61983b69a8ed62289971
phuchau.tea <phuchau.tea@gmail.com>
2023-10-20 23:40:56 +0700

    🍻 Add Base Entity

---
+++ UniXiangqi.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UniXiangqi.Domain.Entities
+{
+    public class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Column(TypeName = "datetime")]
+        public DateTime? CreatedAt { get; set; } = DateTime.Now;
+
+        [Column(TypeName = "datetime")]
+        public DateTime? UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
