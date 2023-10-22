ba54b5c283c2944690bb1826b38a9075ee0b268b
phuchau.tea <phuchau.tea@gmail.com>
2023-10-22 13:16:28 +0700

    🍻 Add Some Entities

---
+++ UniXiangqi.Domain/Entities/UserInRoom.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using UniXiangqi.Domain.Identity;
+
+namespace UniXiangqi.Domain.Entities
+{
+    [Table("UserInRooms")]
+    public class UserInRoom : BaseEntity
+    {
+        public string? UserId { get; set; }
+        public virtual ApplicationUser? User { get; set; }
+        public string? RoomId { get; set; }
+        public virtual Room? Room { get; set; }
+        public bool IsPlayer { get; set; } = false;
+    }
+}
