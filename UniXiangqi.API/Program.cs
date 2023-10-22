872066667ed2fc3e4966c09b7a5c08bf43838710
phuchau.tea <phuchau.tea@gmail.com>
2023-10-22 20:16:15 +0700

    🚀 Game Hub

--- UniXiangqi.API/Program.cs
+++ UniXiangqi.API/Program.cs
@@ -4,6 +4,11 @@ using UniXiangqi.Application.Interfaces;
 using UniXiangqi.Domain.Identity;
 using UniXiangqi.Infrastructure.Persistence;
 using UniXiangqi.Infrastructure.Services;
+using UniXiangqi.API.Hubs;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -23,7 +28,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
-
+builder.Services.AddSignalR();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -32,11 +37,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
+app.UseCors(x => x
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(origin => true) // allow any origin
+                .AllowCredentials()); // allow credentials
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<GameHub>("hubs/game");
 
 app.Run();
