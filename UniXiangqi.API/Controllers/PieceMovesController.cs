59f97467d1a4d42454170260164a660d71decc4f
Le Luc <Letrantanluc2002@gmail.com>
2023-10-23 11:32:31 +0700

    ✨ Add API PieceMoves

---
+++ UniXiangqi.API/Controllers/PieceMovesController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UniXiangqi.Application.DTOs.PieceMove;
+using UniXiangqi.Application.Interfaces;
+
+namespace UniXiangqi.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PieceMovesController : ControllerBase
+    {
+        private IPieceMoveService pieceMoveService;
+        public PieceMovesController(IPieceMoveService pieceMoveService)
+        {
+            this.pieceMoveService = pieceMoveService;
+        }
+        [HttpPost]
+        [Route("/pieceMoves")]
+        public async Task<IActionResult> Create([FromBody] CreatePieceMoveDto pieceMoveDto)
+        {
+            try
+            {
+                var result = await pieceMoveService.Create(pieceMoveDto);
+
+                if (result.statusCode == 1)
+                {
+                    return Ok(new { Message = result.message, RoomId = result.PieceMoveId });
+                }
+                else
+                {
+                    return BadRequest(new { Message = result.message, Error = result.PieceMoveId });
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Message = "Internal Server Error", Error = ex.Message });
+            }
+        }
+
+        [HttpGet]
+        [Route("/pieceMoves")]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var result = await pieceMoveService.GetAll();
+                if (result.statusCode == 1)
+                {
+                    return Ok(new { Message = result.message, rooms = result.pieceMoves });
+                }
+                else
+                {
+                    return BadRequest(new { Message = result.message });
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Message = "Internal Server Error", Error = ex.Message });
+            }
+        }
+        [HttpGet]
+        [Route("/pieceMoves/{matchId}")]
+        public async Task<IActionResult> GetByCode(string matchId)
+        {
+            try
+            {
+                var result = await pieceMoveService.GetByMatchId(matchId);
+                if (result.statusCode == 1)
+                {
+                    return Ok(new { Message = result.message, room = result.pieceMoves });
+                }
+                else
+                {
+                    return BadRequest(new { Message = result.message });
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Message = "Internal Server Error", Error = ex.Message });
+            }
+        }
+    }
+}
